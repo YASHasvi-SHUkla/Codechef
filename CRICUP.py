72df0ddf4a7484a5b8b69326e03580fbfff14785
YASHasvi SHUkla <72089033+YASHasvi-SHUkla@users.noreply.github.com>
2022-07-19 19:28:54 +0530

    Delete CRICUP.py

--- CRICUP.py
+++
@@ -1,9 +0,0 @@
-# cook your dish here
-
-t = int(input())
-for i in range(t):
-    x,y,d = input().split()
-    if abs(int(x) - int(y)) <= int(d):
-        print("Yes")
-    else:
-        print("No")
