58551ec7a899805757df2889288e4e69f662c721
YASHasvi SHUkla <72089033+YASHasvi-SHUkla@users.noreply.github.com>
2022-07-19 21:41:17 +0530

    Janmansh and Assignments
    Janmansh and Assignments
    Janmansh has to submit 3 assignments for Chingari before 10 pm and he starts to do the assignments at X pm. Each assignment takes him 1 hour to complete. Can you tell whether he'll be able to complete all assignments on time or not?
    
    Input Format
    The first line will contain T - the number of test cases. Then the test cases follow.
    The first and only line of each test case contains one integer X - the time when Janmansh starts doing the assignemnts.
    Output Format
    For each test case, output Yes if he can complete the assignments on time. Otherwise, output No.
    
    You may print each character of Yes and No in uppercase or lowercase (for example, yes, yEs, YES will be considered identical).
    
    Constraints
    1≤T≤10
    1≤X≤9
    Sample Input 1 
    2
    7
    9
    Sample Output 1 
    Yes
    No
    
    Explanation
    Test case-1: He can start at 7pm and finish by 10 pm. Therefore he can complete the assignments.
    
    Test case-2: He can not complete all the 3 assignments if he starts at 9 pm

---
+++ Codes/JASSIGNMENTS.py
@@ -0,0 +1,10 @@
+# cook your dish her
+# taking the number of test cases as input
+t = int(input())
+
+for i in range(t):
+    n = int(input())    #  taking each input- the time of starting
+    if n+3 <= 10:
+        print("Yes")
+    else:
+        print("No")
