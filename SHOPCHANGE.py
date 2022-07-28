6c03d27841b47e5974f1d4076e402c07bd33e05e
YASHasvi SHUkla <72089033+YASHasvi-SHUkla@users.noreply.github.com>
2022-07-28 22:24:18 +0530

    Delete SHOPCHANGE.py

--- SHOPCHANGE.py
+++
@@ -1,8 +0,0 @@
-# cook your dish here
-# taking number of test cases as input
-
-t = int(input())
-
-for i in range(t):
-    n = int(input())         #  taking each test case input
-    print(100-n)
